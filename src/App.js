6d63516add8bc105ccf435039bf2b0bea67a51c6
Patrik Persson <Patrik.2.Persson@sigma.se>
2018-01-23 09:38:41 +0100

    update README and update code

--- src/App.js
+++ src/App.js
@@ -3,147 +3,146 @@ import './App.css';
 
 class App extends React.Component {
 
-        constructor() {
-          super();
-          this.addTodo = this.addTodo.bind(this);
-          this.removeTodo = this.removeTodo.bind(this);
-          this.editTodo = this.editTodo.bind(this);
-          this.updateTodo = this.updateTodo.bind(this);  
-          this.state = {
-            todos: [],
-            title: 'ToDos',
-            counter: 0,
-          }
-        }
+  constructor() {
+    super();
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.editTodo = this.editTodo.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);  
+    this.state = {
+      todos: [],
+      title: 'ToDos',
+      counter: 0,
+    }
+  }
 
 
-        updateTodo(event,index) {
-          this.setState({editTodo: false});
-          event.preventDefault();
+  updateTodo(event,index) {
+    this.setState({editTodo: false});
+    event.preventDefault();
 
-          let todos = this.state.todos;
+    let todos = this.state.todos;
 
-          let todoTitle = this.refs.todoTitle.value;
-          let todoYear = this.refs.todoYear.value;
+    let todoName = this.refs.todoName.value;
+    let todoDone = this.refs.todoDone.value;
 
-          let counter = this.state.todo;
+    let counter = this.state.todo;
 
-          let newTodo = {
-            todoTitle,
-            todoYear,
-            counter
-          };
+    let newTodo = {
+      todoName,
+      todoDone,
+      counter
+    };
 
-          todos[counter] = newTodo;
+    todos[counter] = newTodo;
 
-          this.setState({
-            todos: todos
-          });
-          this.refs.todoForm.reset();
-        }
-
-
-        editTodo(index) {
-          this.setState({editTodo: true});
-          let todos = this.state.todos;
-
-          let todo = todos.findIndex(function(todo) {
-            return todo.counter === index
-          });
-
-          this.refs.todoTitle.value = todos[todo].todoTitle;
-          this.refs.todoYear.value = todos[todo].todoYear;
-
-          this.setState({
-            todos: todos,
-            todo: todo
-          });
-        }
+    this.setState({
+      todos: todos
+    });
+    this.refs.todoForm.reset();
+  }
 
 
-        removeTodo(index) {
-          let todos = this.state.todos;
+  editTodo(index) {
+    this.setState({editTodo: true});
+    let todos = this.state.todos;
 
-          let todo = todos.findIndex(function(todo) { 
-            return todo.counter === index 
-          });
+    let todo = todos.findIndex(function(todo) {
+      return todo.counter === index
+    });
+
+    this.refs.todoName.value = todos[todo].todoName;
+    this.refs.todoDone.value = todos[todo].todoDone;
+
+    this.setState({
+      todos: todos,
+      todo: todo
+    });
+  }
+
+
+  removeTodo(index) {
+    let todos = this.state.todos;
+
+    let todo = todos.findIndex(function(todo) { 
+      return todo.counter === index 
+    });
           
-          todos.splice(todo, 1);
+    todos.splice(todo, 1);
 
-          this.setState({
-            todos: todos
-          });
-        }
+    this.setState({
+      todos: todos
+    });
+  }
 
 
-        addTodo(event) {
-          event.preventDefault();
-          let todoTitle = this.refs.todoTitle.value;
-          let todoYear = this.refs.todoYear.value;
-          let counter = this.state.counter;
-          let todo = {
-            todoTitle,
-            todoYear,
-            counter
-          };
-          counter+=1;
-          let todos = this.state.todos;
+  addTodo(event) {
+    event.preventDefault();
+    let todoName = this.refs.todoName.value;
+    let todoDone = this.refs.todoDone.value;
+    let counter = this.state.counter;
+    let todo = {
+      todoName,
+      todoDone,
+      counter
+    };
+    counter+=1;
+    let todos = this.state.todos;
           
-          todos.push(todo);
+    todos.push(todo);
 
-          this.setState({
-            todos: todos,
-            counter: counter
-          });
-           this.refs.todoForm.reset();
-        }
+    this.setState({
+      todos: todos,
+      counter: counter
+    });
+    this.refs.todoForm.reset();
+  }
 
        
-        renderNormal() {
-          let title = this.state.title;
-          let todos = this.state.todos;
-          return (
-            <div className="flex-container">
-              <h1>{title}</h1>
-              <form ref="todoForm">
-                <input type="text" ref="todoTitle" onBlur={(event => {this.setState ({todoTitle: event.target.value})})} placeholder="What Todo?" />
-                <input type="text" ref="todoYear" onBlur={(event => {this.setState ({todoYear: event.target.value})})} placeholder="Is It Done Yet" />
-                <button onClick={this.addTodo}>Add Movie</button>
-              </form>
-              <ul>
-                {todos.map((todo => <li key={todo.counter}><span>Titel: <span>{todo.todoTitle}</span> Done Yet? <span>{todo.todoYear}</span></span> 
-                <button onClick={this.removeTodo.bind(null, todo.counter)}>Delete</button>
-                <button onClick={this.editTodo.bind(null, todo.counter)}>Edit</button>
-                </li>))}         
-              </ul>
-            </div>
-          );
-        }
+  renderNormal() {
+    let title = this.state.title;
+    let todos = this.state.todos;
+    return (
+      <div className="flex-container">
+        <h1>{title}</h1>
+        <form ref="todoForm">
+          <input type="text" ref="todoName" onBlur={(event => {this.setState ({todoName: event.target.value})})} placeholder="What Todo?" />
+          <input type="text" ref="todoDone" onBlur={(event => {this.setState ({todoDone: event.target.value})})} placeholder="Is It Done Yet" />
+          <button onClick={this.addTodo}>Add ToDo</button>
+        </form>
+        <ul>
+          {todos.map((todo => <li key={todo.counter}><span>What ToDo? <span>{todo.todoName}</span> Done Yet? <span>{todo.todoDone}</span></span> 
+          <button onClick={this.removeTodo.bind(null, todo.counter)}>Delete ToDo</button>
+          <button onClick={this.editTodo.bind(null, todo.counter)}>Edit ToDo</button>
+          </li>))}         
+        </ul>
+      </div>
+    );
+  }
         
 
-        renderForm() {
-          let title = this.state.title;
-          let todo = this.state.todo;
-          return (
-            <div className="flex-container">
-              <h1>{title}</h1>
-              <form ref="todoForm">
-                <input type="text" ref="todoTitle" placeholder="What Todo?" />
-                <input type="text" ref="todoYear" placeholder="Is It Done Yet?"/>
-                <button onClick={this.updateTodo}>Update Movie</button>
-              </form>
-            </div>
-          );
-        }
+  renderForm() {
+    let title = this.state.title;
+    return (
+      <div className="flex-container">
+        <h1>{title}</h1>
+        <form ref="todoForm">
+          <input type="text" ref="todoName" placeholder="What Todo?" />
+          <input type="text" ref="todoDone" placeholder="Is It Done Yet?"/>
+          <button onClick={this.updateTodo}>Update ToDo</button>
+        </form>
+      </div>
+    );
+  }
 
 
-        render() {
-          if(this.state.editTodo){
-            return this.renderForm();
-          }else{
-            return this.renderNormal();
-          }
-        }
-      }
+  render() {
+    if(this.state.editTodo){
+      return this.renderForm();
+    }else{
+      return this.renderNormal();
+    }
+  }
+}
 
 export default App;
